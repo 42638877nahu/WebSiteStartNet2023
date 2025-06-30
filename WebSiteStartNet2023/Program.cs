d0bd53c9921336ba00feb10b3658f0b38657eb9e
Facundo Barrera <facubarrera123@gmail.com>
2025-06-30 15:39:43 -0300

    Soporte Y Ayuda (Links y Modal)

--- WebSiteStartNet2023/Program.cs
+++ WebSiteStartNet2023/Program.cs
@@ -8,7 +8,8 @@ using WebSiteStartNet2023.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
