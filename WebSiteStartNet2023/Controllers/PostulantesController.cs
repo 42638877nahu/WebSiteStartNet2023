3256cd35009d447665a751de736e29fd2f42ea50
42638877nahu <ferbuffa@hotmail.com.ar>
2026-01-28 20:40:50 -0300

    Ajustes - Controlers y vistas para :cvs, ect

---
+++ WebSiteStartNet2023/Controllers/PostulantesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebSiteStartNet2023.Data;
+using WebSiteStartNet2023.Models;
+
+namespace WebSiteStartNet2023.Controllers
+{
+    public class PostulantesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostulantesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Postulantes
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Postulantes.Include(p => p.Localidad);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Postulantes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Postulantes == null)
+            {
+                return NotFound();
+            }
+
+            var postulante = await _context.Postulantes
+                .Include(p => p.Localidad)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (postulante == null)
+            {
+                return NotFound();
+            }
+
+            return View(postulante);
+        }
+
+        // GET: Postulantes/Create
+        public IActionResult Create()
+        {
+            ViewData["LocalidadId"] = new SelectList(_context.Localidades, "Id", "Id");
+            return View();
+        }
+
+        // POST: Postulantes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Apellido,DNI,FechaNacimiento,LocalidadId,CodigoArea,TelefonoCelular,Email,ArchivoCV,FechaAlta")] Postulante postulante)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(postulante);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LocalidadId"] = new SelectList(_context.Localidades, "Id", "Id", postulante.LocalidadId);
+            return View(postulante);
+        }
+
+        // GET: Postulantes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Postulantes == null)
+            {
+                return NotFound();
+            }
+
+            var postulante = await _context.Postulantes.FindAsync(id);
+            if (postulante == null)
+            {
+                return NotFound();
+            }
+            ViewData["LocalidadId"] = new SelectList(_context.Localidades, "Id", "Id", postulante.LocalidadId);
+            return View(postulante);
+        }
+
+        // POST: Postulantes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Apellido,DNI,FechaNacimiento,LocalidadId,CodigoArea,TelefonoCelular,Email,ArchivoCV,FechaAlta")] Postulante postulante)
+        {
+            if (id != postulante.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(postulante);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostulanteExists(postulante.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LocalidadId"] = new SelectList(_context.Localidades, "Id", "Id", postulante.LocalidadId);
+            return View(postulante);
+        }
+
+        // GET: Postulantes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Postulantes == null)
+            {
+                return NotFound();
+            }
+
+            var postulante = await _context.Postulantes
+                .Include(p => p.Localidad)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (postulante == null)
+            {
+                return NotFound();
+            }
+
+            return View(postulante);
+        }
+
+        // POST: Postulantes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Postulantes == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Postulantes'  is null.");
+            }
+            var postulante = await _context.Postulantes.FindAsync(id);
+            if (postulante != null)
+            {
+                _context.Postulantes.Remove(postulante);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostulanteExists(int id)
+        {
+          return (_context.Postulantes?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ WebSiteStartNet2023/Data/Migrations/20260128222838_diccionario.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebSiteStartNet2023.Data.Migrations
+{
+    public partial class diccionario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExperienciasTrabajo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExperienciasTrabajo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Idiomas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Idiomas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NivelesConocimiento",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NivelesConocimiento", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Provincias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Provincias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tecnologias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tecnologias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Localidades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CodigoPostal = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProvinciaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Localidades", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Localidades_Provincias_ProvinciaId",
+                        column: x => x.ProvinciaId,
+                        principalTable: "Provincias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Postulantes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DNI = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LocalidadId = table.Column<int>(type: "int", nullable: false),
+                    CodigoArea = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TelefonoCelular = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ArchivoCV = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaAlta = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Postulantes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Postulantes_Localidades_LocalidadId",
+                        column: x => x.LocalidadId,
+                        principalTable: "Localidades",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IdiomasPostulantes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PostulanteId = table.Column<int>(type: "int", nullable: false),
+                    IdiomaId = table.Column<int>(type: "int", nullable: false),
+                    NivelOralId = table.Column<int>(type: "int", nullable: false),
+                    NivelEscrituraId = table.Column<int>(type: "int", nullable: false),
+                    NivelLecturaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdiomasPostulantes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IdiomasPostulantes_Idiomas_IdiomaId",
+                        column: x => x.IdiomaId,
+                        principalTable: "Idiomas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IdiomasPostulantes_Postulantes_PostulanteId",
+                        column: x => x.PostulanteId,
+                        principalTable: "Postulantes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PuestosTrabajoPostulantes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PostulanteId = table.Column<int>(type: "int", nullable: false),
+                    PuestoTrabajoId = table.Column<int>(type: "int", nullable: false),
+                    ExperienciaTrabajoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PuestosTrabajoPostulantes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PuestosTrabajoPostulantes_ExperienciasTrabajo_ExperienciaTrabajoId",
+                        column: x => x.ExperienciaTrabajoId,
+                        principalTable: "ExperienciasTrabajo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PuestosTrabajoPostulantes_Postulantes_PostulanteId",
+                        column: x => x.PostulanteId,
+                        principalTable: "Postulantes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PuestosTrabajoPostulantes_PuestoTrabajos_PuestoTrabajoId",
+                        column: x => x.PuestoTrabajoId,
+                        principalTable: "PuestoTrabajos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TecnologiasPostulantes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PostulanteId = table.Column<int>(type: "int", nullable: false),
+                    TecnologiaId = table.Column<int>(type: "int", nullable: false),
+                    NivelConocimientoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TecnologiasPostulantes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TecnologiasPostulantes_NivelesConocimiento_NivelConocimientoId",
+                        column: x => x.NivelConocimientoId,
+                        principalTable: "NivelesConocimiento",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TecnologiasPostulantes_Postulantes_PostulanteId",
+                        column: x => x.PostulanteId,
+                        principalTable: "Postulantes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TecnologiasPostulantes_Tecnologias_TecnologiaId",
+                        column: x => x.TecnologiaId,
+                        principalTable: "Tecnologias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdiomasPostulantes_IdiomaId",
+                table: "IdiomasPostulantes",
+                column: "IdiomaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdiomasPostulantes_PostulanteId",
+                table: "IdiomasPostulantes",
+                column: "PostulanteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Localidades_ProvinciaId",
+                table: "Localidades",
+                column: "ProvinciaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Postulantes_LocalidadId",
+                table: "Postulantes",
+                column: "LocalidadId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PuestosTrabajoPostulantes_ExperienciaTrabajoId",
+                table: "PuestosTrabajoPostulantes",
+                column: "ExperienciaTrabajoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PuestosTrabajoPostulantes_PostulanteId",
+                table: "PuestosTrabajoPostulantes",
+                column: "PostulanteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PuestosTrabajoPostulantes_PuestoTrabajoId",
+                table: "PuestosTrabajoPostulantes",
+                column: "PuestoTrabajoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TecnologiasPostulantes_NivelConocimientoId",
+                table: "TecnologiasPostulantes",
+                column: "NivelConocimientoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TecnologiasPostulantes_PostulanteId",
+                table: "TecnologiasPostulantes",
+                column: "PostulanteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TecnologiasPostulantes_TecnologiaId",
+                table: "TecnologiasPostulantes",
+                column: "TecnologiaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IdiomasPostulantes");
+
+            migrationBuilder.DropTable(
+                name: "PuestosTrabajoPostulantes");
+
+            migrationBuilder.DropTable(
+                name: "TecnologiasPostulantes");
+
+            migrationBuilder.DropTable(
+                name: "Idiomas");
+
+            migrationBuilder.DropTable(
+                name: "ExperienciasTrabajo");
+
+            migrationBuilder.DropTable(
+                name: "NivelesConocimiento");
+
+            migrationBuilder.DropTable(
+                name: "Postulantes");
+
+            migrationBuilder.DropTable(
+                name: "Tecnologias");
+
+            migrationBuilder.DropTable(
+                name: "Localidades");
+
+            migrationBuilder.DropTable(
+                name: "Provincias");
+        }
+    }
+}
