e00233ae0523bf867120cca02dfce8f986521ef4
42638877nahu <ferbuffa@hotmail.com.ar>
2026-02-04 23:28:22 -0300

    relaciones postulantes/ajustes visuales en el Create de Postulantes

---
+++ WebSiteStartNet2023/Data/Migrations/20260205022440_RelacionesPostulante.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebSiteStartNet2023.Data.Migrations
+{
+    public partial class RelacionesPostulante : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NivelConocimientoId",
+                table: "Postulantes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProvinciaId",
+                table: "Postulantes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PuestoTrabajoId",
+                table: "Postulantes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PostulanteId",
+                table: "NivelesConocimiento",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NivelesConocimiento_PostulanteId",
+                table: "NivelesConocimiento",
+                column: "PostulanteId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NivelesConocimiento_Postulantes_PostulanteId",
+                table: "NivelesConocimiento",
+                column: "PostulanteId",
+                principalTable: "Postulantes",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NivelesConocimiento_Postulantes_PostulanteId",
+                table: "NivelesConocimiento");
+
+            migrationBuilder.DropIndex(
+                name: "IX_NivelesConocimiento_PostulanteId",
+                table: "NivelesConocimiento");
+
+            migrationBuilder.DropColumn(
+                name: "NivelConocimientoId",
+                table: "Postulantes");
+
+            migrationBuilder.DropColumn(
+                name: "ProvinciaId",
+                table: "Postulantes");
+
+            migrationBuilder.DropColumn(
+                name: "PuestoTrabajoId",
+                table: "Postulantes");
+
+            migrationBuilder.DropColumn(
+                name: "PostulanteId",
+                table: "NivelesConocimiento");
+        }
+    }
+}
--- WebSiteStartNet2023/Models/Postulante.cs
+++ WebSiteStartNet2023/Models/Postulante.cs
@@ -17,6 +17,8 @@ namespace WebSiteStartNet2023.Models
         public string DNI { get; set; }
 
         public DateTime FechaNacimiento { get; set; }
+        public int ProvinciaId { get; set; }
+        //public Provincia Provincia { get; set; }
 
         public int LocalidadId { get; set; }
         public Localidad Localidad { get; set; }
@@ -26,11 +28,15 @@ namespace WebSiteStartNet2023.Models
 
         [EmailAddress]
         public string Email { get; set; }
+        public int NivelConocimientoId { get; set; }
+
+        public int PuestoTrabajoId { get; set; }
 
         public string ArchivoCV { get; set; }
         public DateTime FechaAlta { get; set; }
 
         // RELACIONES
+        public ICollection<NivelConocimiento> NivelConocimientos { get; set; }
         public ICollection<TecnologiaPostulante> Tecnologias { get; set; }
         public ICollection<IdiomaPostulante> Idiomas { get; set; }
         public ICollection<PuestoTrabajoPostulante> PuestosTrabajo { get; set; }
